e96f6052c36393a5b8c6701ed72f9bc167dd5467
Eric O'Connell <eric.oconnell@idealist.org>
2015-02-04 17:39:28 -0800

    Bump to React 0.13b

--- server/app.jsx
+++ server/app.jsx
@@ -13,15 +13,9 @@ function globalScriptAssignment(name, value) {
 async function handleRequest(req, res, next) {
     let posts = await model.Post.getAll();
     Router.run(Routes, req.path, (Handler) => {
-        var appMarkup = React.renderToString(
-            React.createElement(Handler, {posts})
-        );
+        var appMarkup = React.renderToString(<Handler posts={posts} />);
         var markup = React.renderToStaticMarkup(
-            React.createElement(
-                Base,
-                {posts: globalScriptAssignment('__posts__', posts)},
-                appMarkup
-            )
+            <Base posts={globalScriptAssignment('__posts__', posts)}>{appMarkup}</Base>
         );
         res.end(markup);
     });
